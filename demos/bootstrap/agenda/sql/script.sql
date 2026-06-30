e2b74e3657962a9607c84e2f1984c2cbd5a70c66
Edher G.O <1725110071@utectulancingo.edu.mx>
2026-06-30 15:48:34 +0000

    UPDATE mostrar filas de sqlite3

--- demos/bootstrap/agenda/sql/script.sql
+++ demos/bootstrap/agenda/sql/script.sql
@@ -1,12 +1,16 @@
-CREATE TABLE conctactos(
+CREATE TABLE contactos(
     id_contacto INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
     primer_apellido TEXT NOT NULL,
     segundo_apellido TEXT NOT NULL,
-    email TECT NOT NULL,
-    telefono TEXT NOT NULL,
-
+    email TEXT NOT NULL,
+    telefono TEXT NOT NULL
 );
 
-INSERT INTO conctactos(nombre,primer_apellido,segundo_apellido,email,telefono)
-("Dejah","Thoris","Barsonn","dejah@email.com","111111111"),
-("John","Carter","Earth","john@email.com","222222222");
+INSERT INTO contactos(nombre,primer_apellido,segundo_apellido,email,telefono)
+VALUES
+('Dejah','Thoris','Barsonn','dejah@email.com','111111111'),
+('John','Carter','Earth','john@email.com','22222222');
+
+
+SELECT * FROM contactos;
